d044fa44166c51869f6075534979a9423aa96d45
Rafael Cardoso <rscedf@gmail.com>
2022-11-10 19:14:36 -0300

    Instalado e configurado styled component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,23 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle `
+  * {
+    padding: 0;
+    margin: 0;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+  }
+`
 
 function App ({ Component, pageProps }) {
   return (
-    <Component {...pageProps} />
+    <>
+      <GlobalStyle/>
+      <Component {...pageProps} />
+    </>
+    
   )
 }
 
