d39c7b6739f88f34a201ee1d2be2ffe17cf62fe9
Rafael Cardoso <rscedf@gmail.com>
2022-11-14 17:35:15 -0300

    criado componente imagem

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageWithSpace from "../src/components/layout/imageWithSpace"
+
+function LoginPage(){
+    return(
+        <ImageWithSpace>
+            teste
+        </ImageWithSpace>
+    )
+}
+
+export default LoginPage 
