b07a540a0ca3843a18569d7754d4fbb1982ab956
Rafael Cardoso <rscedf@gmail.com>
2022-11-21 11:16:31 -0300

    criado serviço autenticação

--- pages/api/user/login.js
+++ pages/api/user/login.js
@@ -1,8 +1,11 @@
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import createHandler from '../../../lib/middleware/nextConnect'
 import validate from '../../../lib/middleware/validate'
 import {login} from '../../../modules/user/user.service'
+
+import { ironConfig } from '../../../lib/middleware/ironSession'
 
 
 const loginSchema = Joi.object({
@@ -16,12 +19,17 @@ const handler = createHandler()
 handler.post(validate({ body: loginSchema }), async (req,res) =>{
     try {
         const user= await login(req.body)
-        res.send(user)
+        req.session.user = {
+            id: user.id,
+            user: user.user
+        }
+        await req.session.save()
+        res.send('ok: true')
     } catch (error) {
         console.error(error)
         throw error
     }
 })
 
-export default handler
+export default withIronSessionApiRoute(handler, ironConfig) 
 
--- pages/api/user/signup.js
+++ pages/api/user/signup.js
@@ -1,11 +1,13 @@
-
 import Joi from 'joi'
+import { withIronSessionApiRoute } from 'iron-session/next'
 
 import createHandler from '../../../lib/middleware/nextConnect'
 
 import validate from '../../../lib/middleware/validate'
 
 import {signupUser} from '../../../modules/user/user.service'
+
+import { ironConfig } from '../../../lib/middleware/ironSession'
 
 const postSchema = Joi.object({
     firstName: Joi.string().required().max(50),
@@ -20,7 +22,13 @@ const signup = createHandler()
 signup.post(validate({ body: postSchema}), async(req, res)=> {  
     try {
         const user = await signupUser(req.body)
-        res.status(201).json({ user })
+        req.session.user = {
+            id: user.id,
+            user: user.user
+        }
+        await req.session.save()
+
+        res.status(201).json({ ok: true })
     } catch (error) {
         console.error(error)
         throw error
@@ -29,4 +37,4 @@ signup.post(validate({ body: postSchema}), async(req, res)=> {
 })
 
 
-export default signup
+export default withIronSessionApiRoute (signup, ironConfig)
