267964b250b5bf06d3cf9e45a56addd04612d342
Rafael Cardoso <rscedf@gmail.com>
2022-11-21 21:29:28 -0300

    criado tratamento de erros cadastro

--- modules/user/user.schema.js
+++ modules/user/user.schema.js
@@ -1,9 +1,11 @@
 import Joi from "joi"
 
 export const signupSchema = Joi.object({
-    firstName: Joi.string().required().max(50),
-    lastName: Joi.string().required().max(50),
-    user: Joi.string().required().max(30),
-    email: Joi.string().email({tlds: {allow:false} }).required().max(100),
-    password: Joi.string().required().max(50).min(6)
+    firstName: Joi.string().required().max(50).message('O campo "nome" pode ter no máximo {{#limit}} caractere'),
+    lastName: Joi.string().required().max(50).message('O campo "sobrenome" pode ter no máximo {{#limit}} caractere'),
+    user: Joi.string().required().max(30).message('O campo "usuário" pode ter no máximo {{#limit}} caractere'),
+    email: Joi.string().email({tlds: {allow:false} }).required().max(100).message('O campo "email" pode ter no máximo {{#limit}} caractere'),
+    password: Joi.string().required() 
+        .max(50).message('O campo "senha" pode ter no máximo {{#limit}} caractere')
+        .min(6).message('O campo "senha" precisa ter no mínimo {{#limit}} caractere')
 })
